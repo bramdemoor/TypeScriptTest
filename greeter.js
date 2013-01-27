c9888984caef84d59b7fe9f1a8d1fc6e46e39903
Bram De Moor <bram.demoor@gmail.com>
2013-01-27 14:29:33 +0100

    Performance fix

--- greeter.js
+++ greeter.js
@@ -12,6 +12,10 @@ var WorldObject = (function () {
         this._x = x;
         this._y = y;
     }
+    WorldObject.prototype.move = function (x, y) {
+        this._x = x;
+        this._y = y;
+    };
     return WorldObject;
 })();
 var Box = (function (_super) {
@@ -22,7 +26,7 @@ var Box = (function (_super) {
         this.y = y;
     }
     Box.prototype.draw = function (c) {
-        c.fillStyle("#ff0000").fillRect(this._x, this._y, 32, 32).blur();
+        c.fillStyle("#ff0000").fillRect(this._x, this._y, 32, 32);
     };
     Box.prototype.update = function () {
         this._y += 1;
@@ -37,20 +41,36 @@ var Bullet = (function (_super) {
         this.y = y;
     }
     Bullet.prototype.draw = function (c) {
-        c.fillStyle("#00ff00").fillRect(this._x, this._y, 8, 4).blur();
+        c.fillStyle("#00ff00").fillRect(this._x, this._y, 8, 4);
     };
     Bullet.prototype.update = function () {
         this._x += 1;
     };
     return Bullet;
 })(WorldObject);
+var CrossHair = (function (_super) {
+    __extends(CrossHair, _super);
+    function CrossHair(x, y) {
+        _super.call(this, x, y);
+        this.x = x;
+        this.y = y;
+    }
+    CrossHair.prototype.draw = function (c) {
+        c.fillStyle("#0000ff").fillRect(this._x, this._y, 16, 16);
+    };
+    return CrossHair;
+})(WorldObject);
 var box = new Box(10, 20);
 var bullet = new Bullet(40, 50);
+var crosshair = new CrossHair(0, 0);
 cq(640, 480).onStep(function (delta, time) {
     box.update();
     bullet.update();
+}).onMouseMove(function (x, y) {
+    crosshair.move(x, y);
 }).onRender(function (delta, time) {
-    this.clearRect(0, 0, 640, 480).fillStyle("#cccccc").fillRect(0, 0, 640, 480);
+    this.save().clear("#220033");
     box.draw(this);
     bullet.draw(this);
+    crosshair.draw(this);
 }).appendTo("body");
--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,5 @@
+declare var cq: any;
+
 class WorldObject {
 	_x: number = 0;
 	_y: number = 0;	
@@ -5,12 +7,16 @@ class WorldObject {
 		this._x = x;
 		this._y = y;
 	}
+	move(x: number, y: number) {
+		this._x = x;
+		this._y = y;	
+	}
 }
 
 class Box extends WorldObject {
 	constructor(public x: number, public y: number) { super(x,y); }
 	draw(c) {
-		c.fillStyle("#ff0000").fillRect(this._x,this. _y, 32, 32).blur();
+		c.fillStyle("#ff0000").fillRect(this._x,this. _y, 32, 32);
 	}
 	update() {
 		this._y += 1;
@@ -20,32 +26,40 @@ class Box extends WorldObject {
 class Bullet extends WorldObject {
 	constructor(public x: number, public y: number) { super(x,y); }
 	draw(c) {
-		c.fillStyle("#00ff00").fillRect(this._x,this. _y, 8, 4).blur();
+		c.fillStyle("#00ff00").fillRect(this._x,this. _y, 8, 4);
 	}
 	update() {
 		this._x += 1;
 	}
 }
 
+class CrossHair extends WorldObject {
+	constructor(public x: number, public y: number) { super(x,y); }
+	draw(c) {
+		c.fillStyle("#0000ff").fillRect(this._x,this. _y, 16, 16);
+	}
+}
+
 // Create our scene
 var box = new Box(10, 20);
 var bullet = new Bullet(40, 50);
-
-declare var cq: any;
+var crosshair = new CrossHair(0, 0);
 
 // Simple game loop
-cq(640,480)
+cq(640, 480)
 	.onStep(function(delta, time) { 
 		box.update();
 		bullet.update();
 	})
+	.onMouseMove(function(x, y) {
+		crosshair.move(x,y);		
+	})	
 	.onRender(function(delta, time) { 
-		this
-			.clearRect(0,0,640,480)
-			.fillStyle("#cccccc")
-			.fillRect(0,0,640,480);
+		
+		this.save().clear("#220033")			
 		box.draw(this);
 		bullet.draw(this);
+		crosshair.draw(this);
 	})
 	.appendTo("body");
 
