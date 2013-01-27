4bc44cf4e57e0fa698d712b22e8dbba2dee80e9a
Bram De Moor <bram.demoor@gmail.com>
2013-01-27 14:14:40 +0100

    Simple CanvasQuery drawing

--- greeter.js
+++ greeter.js
@@ -1,11 +1,47 @@
+var __extends = this.__extends || function (d, b) {
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
 var WorldObject = (function () {
-    function WorldObject() { }
+    function WorldObject(x, y) {
+        this.x = x;
+        this.y = y;
+        this._x = 0;
+        this._y = 0;
+        this._x = x;
+        this._y = y;
+    }
     WorldObject.prototype.update = function () {
         console.log('update!');
     };
     return WorldObject;
 })();
-var thing = new WorldObject();
-thing.update();
-thing.update();
-thing.update();
+var Bomb = (function (_super) {
+    __extends(Bomb, _super);
+    function Bomb(x, y) {
+        _super.call(this, x, y);
+        this.x = x;
+        this.y = y;
+    }
+    Bomb.prototype.draw = function (c) {
+        c.fillStyle("#ff0000").fillRect(this._x, this._y, 32, 32).blur();
+    };
+    return Bomb;
+})(WorldObject);
+var Bullet = (function (_super) {
+    __extends(Bullet, _super);
+    function Bullet(x, y) {
+        _super.call(this, x, y);
+        this.x = x;
+        this.y = y;
+    }
+    return Bullet;
+})(WorldObject);
+var bomb = new Bomb(10, 20);
+cq(640, 480).onStep(function (delta, time) {
+    bomb.update();
+}).onRender(function (delta, time) {
+    this.clearRect(0, 0, 640, 480).fillStyle("#cccccc").fillRect(0, 0, 640, 480);
+    bomb.draw(this);
+}).appendTo("body");
--- greeter.ts
+++ greeter.ts
@@ -1,14 +1,43 @@
-interface Updatable {   
-   update(): void;     
-}
-
-class WorldObject implements Updatable {
+class WorldObject {
+	_x: number = 0;
+	_y: number = 0;	
+	constructor(public x: number, public y: number) {
+		this._x = x;
+		this._y = y;
+	}
 	public update() {
 		console.log('update!');
 	}
 }
 
-var thing = new WorldObject();
-thing.update();
-thing.update();
-thing.update();
+class Bomb extends WorldObject {
+	constructor(public x: number, public y: number) { super(x,y); }
+	draw(c) {
+		c.fillStyle("#ff0000").fillRect(this._x,this. _y, 32, 32).blur()
+	}
+}
+
+class Bullet extends WorldObject {
+	constructor(public x: number, public y: number) { super(x,y); }
+}
+
+// Create our scene
+var bomb = new Bomb(10, 20);
+
+declare var cq: any;
+
+// Simple game loop
+cq(640,480)
+	.onStep(function(delta, time) { 
+		bomb.update();
+	})
+	.onRender(function(delta, time) { 
+		this
+			.clearRect(0,0,640,480)
+			.fillStyle("#cccccc")
+			.fillRect(0,0,640,480);
+		bomb.draw(this);
+	})
+	.appendTo("body");
+
+
